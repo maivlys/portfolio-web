3c2359873bbabefb79047bbc3ddd1bdb66f1d0bd
maivlys <122049862+maivlys@users.noreply.github.com>
2026-02-02 18:30:10 +0100

    footer gui, no functionality

---
+++ src/Footer.jsx
@@ -0,0 +1,24 @@
+import styles from "./Footer.module.css";
+
+export function Footer(params) {
+  return (
+    <div className={styles.container}>
+      <footer className={styles.footer}>
+        <div className={styles.left}>
+          <button className={styles.btn}>about</button>
+          <button className={styles.btn}>my work</button>
+          <button className={styles.btn}>contact</button>
+        </div>
+        <div className={styles.right}>
+          <img
+            className={styles.img}
+            src="../public/signature/dark-mode/signature.svg"
+            alt=""
+          />
+          <p className={styles.year}>2026</p>
+        </div>
+      </footer>
+      <div className={styles.border}></div>
+    </div>
+  );
+}
