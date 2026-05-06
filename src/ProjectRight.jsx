239ae679545caa67dd182c2b7443c47365afa9e9
maivlys <122049862+maivlys@users.noreply.github.com>
2026-05-06 16:41:35 +0200

    projects layout correction, projects data file mangment

--- src/ProjectRight.jsx
+++ src/ProjectRight.jsx
@@ -2,58 +2,71 @@ import styles from "./ProjectRight.module.css";
 import animationStyle from "./index.module.css";
 
 export function ProjectRight({ project }) {
+  const inDevelopment = project?.description === "" ? true : false;
+
   return (
     <div className={styles.container}>
       <div className={`${styles.left} ${animationStyle.block}`}>
         <p className={styles.p}>project {project?.number}</p>
         <h4>{project?.title}</h4>
-        <p className={styles.desc}>{project?.description}</p>
+        <p className={`${styles.desc} ${inDevelopment ? styles.empty : ""}`}>
+          {inDevelopment ? "coming soon" : project?.description}
+        </p>
         <div className={styles.links}>
-          <button className={styles.btn}>
-            <a href={project?.githubURL} target="_blank">
-              github
-              <svg
-                className={styles.svg}
-                width={12}
-                height={12}
-                fill="none"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={1}
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M7 17 17 7" />
-                <path d="M7 7h10v10" />
-              </svg>
-            </a>
-          </button>
-          <button className={styles.btn}>
-            <a href={project?.demoURL} target="_blank">
-              live demo
-              <svg
-                className={styles.svg}
-                width={12}
-                height={12}
-                fill="none"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={1}
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M7 17 17 7" />
-                <path d="M7 7h10v10" />
-              </svg>
-            </a>
-          </button>
+          {project?.githubURL && (
+            <button className={styles.btn}>
+              <a href={project?.githubURL} target="_blank">
+                github
+                <svg
+                  className={styles.svg}
+                  width={12}
+                  height={12}
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={1}
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M7 17 17 7" />
+                  <path d="M7 7h10v10" />
+                </svg>
+              </a>
+            </button>
+          )}
+
+          {project?.demoURL && (
+            <button className={styles.btn}>
+              <a href={project?.demoURL} target="_blank">
+                live demo
+                <svg
+                  className={styles.svg}
+                  width={12}
+                  height={12}
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={1}
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M7 17 17 7" />
+                  <path d="M7 7h10v10" />
+                </svg>
+              </a>
+            </button>
+          )}
         </div>
       </div>
       <div className={`${styles.right} ${animationStyle.block}`}>
         <div className={styles.border}>
-          <a href={project?.demoURL} target="_blank">
+          {inDevelopment ? (
             <img className={styles.img} src={project?.imgSrc} alt="" />
-          </a>
+          ) : (
+            <a href={project?.demoURL} target="_blank" rel="noreferrer">
+              <img className={styles.img} src={project?.imgSrc} alt="" />
+            </a>
+          )}
         </div>
       </div>
     </div>
