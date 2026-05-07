eb519a9d6bdfa40a4ea66f722b06b53bb4ae3a21
maivlys <122049862+maivlys@users.noreply.github.com>
2026-05-07 20:44:57 +0200

    about me section text added ad adjusted-size and line height

--- src/Intro.jsx
+++ src/Intro.jsx
@@ -12,13 +12,10 @@ export function Intro(params) {
         <div className={`${styles.left} ${animationStyle.block}`}>
           <p className={styles.p}>who am i</p>
           <p className={styles.desc}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+            A frontend developer who speaks design and values structured logic.
+            I bridge “the nice” with “the functional”. Currently collaborating
+            globally, thinking locally, and obsessed with the little details
+            most people miss. Also slightly obsessed with The Office.
           </p>
         </div>
 
